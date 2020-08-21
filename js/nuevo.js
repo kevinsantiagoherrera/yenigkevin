b4f7f927e3b39e1aaed7178c7ff770bff9b5a93f
andres felipe <anfehergar@gmail.com>
2020-08-21 18:06:23 -0500

    subiendo los primeros archivos

---
+++ js/nuevo.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+var _hsq = _hsq || [];
+_hsq.push(["setContentType", "standard-page"]);
+_hsq.push(["setCanonicalUrl", "https:\/\/www.fracttal.com\/solicitar-demo-gratis-es"]);
+_hsq.push(["setPageId", "9157540957"]);
+_hsq.push(["setContentMetadata", {
+    "contentPageId": 9157540957,
+    "legacyPageId": "9157540957",
+    "contentFolderId": null,
+    "contentGroupId": null,
+    "abTestId": null,
+    "languageVariantId": 9157540957,
+    "languageCode": "es-cl",
+    
+}]);
+</script>
